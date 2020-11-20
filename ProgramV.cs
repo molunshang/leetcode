acfb0ff33d00fb2f7e6e4d99a4473808fb4825cd
郑凯 <kai.zheng@fengjr.com>
2020-11-20 18:58:47 +0800

    147. 对链表进行插入排序
    Signed-off-by: 郑凯 <kai.zheng@fengjr.com>

--- ProgramV.cs
+++ ProgramV.cs
@@ -332,7 +332,7 @@ namespace leetcode
                 break;
             }
 
-            return new[] { duplicate, miss };
+            return new[] {duplicate, miss};
         }
 
         #endregion
@@ -487,12 +487,12 @@ namespace leetcode
                 int t1 = num + k, t2 = num - k;
                 if (set.Contains(t1))
                 {
-                    numSet.Add(new[] { num, t1 });
+                    numSet.Add(new[] {num, t1});
                 }
 
                 if (set.Contains(t2))
                 {
-                    numSet.Add(new[] { t2, num });
+                    numSet.Add(new[] {t2, num});
                 }
 
                 set.Add(num);
@@ -989,7 +989,7 @@ namespace leetcode
         {
             if (n == 1)
             {
-                return new[] { 0 };
+                return new[] {0};
             }
 
             var graph = new Dictionary<int, List<int>>();
@@ -2150,7 +2150,7 @@ namespace leetcode
                 while (reader.Peek() > -1)
                 {
                     //read int
-                    var ch = (char)reader.Read();
+                    var ch = (char) reader.Read();
                     switch (ch)
                     {
                         case ']':
@@ -2159,7 +2159,7 @@ namespace leetcode
                             nestedInteger.Add(Read());
                             break;
                         case '[':
-                            if (']' == (char)reader.Peek())
+                            if (']' == (char) reader.Peek())
                             {
                                 reader.Read();
                                 return nestedInteger;
@@ -2171,7 +2171,7 @@ namespace leetcode
                             numStr.Append(ch);
                             while (reader.Peek() > -1)
                             {
-                                ch = (char)reader.Peek();
+                                ch = (char) reader.Peek();
                                 if (ch != '-' && !char.IsDigit(ch))
                                 {
                                     break;
@@ -2270,7 +2270,7 @@ namespace leetcode
             /** Get a random element from the collection. */
             public int GetRandom()
             {
-                return data[(int)(random.NextDouble() * data.Count)];
+                return data[(int) (random.NextDouble() * data.Count)];
             }
         }
 
@@ -2591,7 +2591,7 @@ namespace leetcode
                     }
                 }
 
-                return new TreeNode(rootVal) { left = BuildTree(l, index - 1), right = BuildTree(index + 1, r) };
+                return new TreeNode(rootVal) {left = BuildTree(l, index - 1), right = BuildTree(index + 1, r)};
             }
 
             return BuildTree(0, nums.Length - 1);
@@ -2695,21 +2695,21 @@ namespace leetcode
                 {
                     if (p1 > mid)
                     {
-                        sorted[p++] = (int)ranges[p2++];
+                        sorted[p++] = (int) ranges[p2++];
                     }
                     else if (p2 > right)
                     {
-                        sorted[p++] = (int)ranges[p1++];
+                        sorted[p++] = (int) ranges[p1++];
                     }
                     else
                     {
                         if (ranges[p1] < ranges[p2])
                         {
-                            sorted[p++] = (int)ranges[p1++];
+                            sorted[p++] = (int) ranges[p1++];
                         }
                         else
                         {
-                            sorted[p++] = (int)ranges[p2++];
+                            sorted[p++] = (int) ranges[p2++];
                         }
                     }
                 }
@@ -3052,7 +3052,7 @@ namespace leetcode
                     ch++;
                 }
 
-                dict[(char)('0' + i)] = chars;
+                dict[(char) ('0' + i)] = chars;
             }
 
             var check = new char[num.Length][];
@@ -3363,11 +3363,11 @@ namespace leetcode
             //最小公倍数
             long Mcm(long n1, long n2)
             {
-                var gcd = Gcd((int)n1, (int)n2);
+                var gcd = Gcd((int) n1, (int) n2);
                 return n1 * n2 / gcd;
             }
 
-            int l = Math.Min(a, Math.Min(b, c)), r = 2 * (int)Math.Pow(10, 9);
+            int l = Math.Min(a, Math.Min(b, c)), r = 2 * (int) Math.Pow(10, 9);
             long ab = Mcm(a, b), ac = Mcm(a, c), bc = Mcm(b, c), abc = Mcm(a, bc);
             while (l <= r)
             {
@@ -3389,20 +3389,24 @@ namespace leetcode
         #endregion
 
         #region 1312. 让字符串成为回文串的最少插入次数
+
         //https://leetcode-cn.com/problems/minimum-insertion-steps-to-make-a-string-palindrome/
         public int MinInsertions(string s)
         {
             var cache = new int?[s.Length, s.Length];
+
             int Dfs(int l, int r)
             {
                 if (l >= r)
                 {
                     return 0;
                 }
+
                 if (cache[l, r].HasValue)
                 {
                     return cache[l, r].Value;
                 }
+
                 var res = 0;
                 if (s[l] == s[r])
                 {
@@ -3412,11 +3416,14 @@ namespace leetcode
                 {
                     res = Math.Min(Dfs(l + 1, r), Dfs(l, r - 1)) + 1;
                 }
+
                 cache[l, r] = res;
                 return res;
             }
+
             return Dfs(0, s.Length - 1);
         }
+
         #endregion
 
         #region 222. 完全二叉树的节点个数
@@ -3431,6 +3438,60 @@ namespace leetcode
             }
 
             return CountNodes(root.left) + CountNodes(root.right) + 1;
+        }
+
+        #endregion
+
+        #region 147. 对链表进行插入排序
+
+        //https://leetcode-cn.com/problems/insertion-sort-list/
+        public ListNode InsertionSortList(ListNode head)
+        {
+            ListNode Insert(ListNode root, ListNode node)
+            {
+                if (root == null)
+                {
+                    return node;
+                }
+
+                ListNode newHead = root, prev = null;
+                while (root != null && root.val < node.val)
+                {
+                    prev = root;
+                    root = root.next;
+                }
+
+                if (root == null)
+                {
+                    prev.next = node;
+                }
+                else
+                {
+                    if (prev == null)
+                    {
+                        node.next = newHead;
+                        newHead = node;
+                    }
+                    else
+                    {
+                        prev.next = node;
+                        node.next = root;
+                    }
+                }
+
+                return newHead;
+            }
+
+            ListNode result = null;
+            while (head != null)
+            {
+                var next = head.next;
+                head.next = null;
+                result = Insert(result, head);
+                head = next;
+            }
+
+            return result;
         }
 
         #endregion
