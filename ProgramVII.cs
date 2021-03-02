9982d89eba4246a8cd5d0088644995452747f8bd
郑凯 <kai.zheng@fengjr.com>
2021-03-02 17:58:31 +0800

    304. 二维区域和检索 - 矩阵不可变
    Signed-off-by: 郑凯 <kai.zheng@fengjr.com>

---
+++ ProgramVII.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetcode
+{
+    partial class Program
+    {
+        #region 304. 二维区域和检索 - 矩阵不可变
+
+        //https://leetcode-cn.com/problems/range-sum-query-2d-immutable/
+        class NumMatrix
+        {
+            private int[,] prefixSums;
+            private int[][] matrix;
+
+            public NumMatrix(int[][] matrix)
+            {
+                this.matrix = matrix;
+                if (matrix.Length <= 0 || matrix[0].Length <= 0)
+                {
+                    return;
+                }
+                int m = matrix.Length, n = matrix[0].Length;
+                prefixSums = new int[m, n];
+                for (int i = 0; i < m; i++)
+                {
+                    var arr = matrix[i];
+                    prefixSums[i, 0] = arr[0];
+                    for (int j = 1; j < n; j++)
+                    {
+                        prefixSums[i, j] = prefixSums[i, j - 1] + arr[j];
+                    }
+                }
+            }
+
+            public int SumRegion(int row1, int col1, int row2, int col2)
+            {
+                if (prefixSums == null)
+                {
+                    return 0;
+                }
+                var sum = 0;
+                for (int i = row1; i <= row2; i++)
+                {
+                    sum += prefixSums[i, col2] - prefixSums[i, col1] + matrix[i][col1];
+                }
+
+                return sum;
+            }
+        }
+
+        #endregion
+    }
+}
