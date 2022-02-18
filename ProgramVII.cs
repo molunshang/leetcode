2c22d3787081bf121ef02f07d2fa4676561a08ef
molunshang <shengwutai@163.com>
2022-02-18 23:40:45 +0800

    1791. 找出星型图的中心节点

--- ProgramVII.cs
+++ ProgramVII.cs
@@ -1259,5 +1259,12 @@ namespace leetcode
         }
         #endregion
 
+        #region 1791. 找出星型图的中心节点
+        //https://leetcode-cn.com/problems/find-center-of-star-graph/
+        public int FindCenter(int[][] edges)
+        {
+            return edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1] ? edges[0][0] : edges[0][1];
+        }
+        #endregion
     }
 }
