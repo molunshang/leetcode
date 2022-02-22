55d4eaa8961c61c97fc3e5709bded002f6568757
molunshang <shengwutai@163.com>
2022-02-22 23:34:40 +0800

    1994. 好子集的数目

--- ProgramVII.cs
+++ ProgramVII.cs
@@ -1353,5 +1353,76 @@ namespace leetcode
             return new string(s);
         }
         #endregion
+
+        #region 1994. 好子集的数目
+        //https://leetcode-cn.com/problems/the-number-of-good-subsets/
+        //TODO  copy代码，后面详细了解
+        static int[] PRIMES = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
+        static int NUM_MAX = 30;
+        static int MOD = 1000000007;
+
+        public int NumberOfGoodSubsets(int[] nums)
+        {
+            int[] freq = new int[NUM_MAX + 1];
+            foreach (int num in nums)
+            {
+                ++freq[num];
+            }
+
+            int[] f = new int[1 << PRIMES.Length];
+            f[0] = 1;
+            for (int i = 0; i < freq[1]; ++i)
+            {
+                f[0] = f[0] * 2 % MOD;
+            }
+
+            for (int i = 2; i <= NUM_MAX; ++i)
+            {
+                if (freq[i] == 0)
+                {
+                    continue;
+                }
+
+                // 检查 i 的每个质因数是否均不超过 1 个
+                int subset = 0, x = i;
+                bool check = true;
+                for (int j = 0; j < PRIMES.Length; ++j)
+                {
+                    int prime = PRIMES[j];
+                    if (x % (prime * prime) == 0)
+                    {
+                        check = false;
+                        break;
+                    }
+                    if (x % prime == 0)
+                    {
+                        subset |= (1 << j);
+                    }
+                }
+                if (!check)
+                {
+                    continue;
+                }
+
+                // 动态规划
+                for (int mask = (1 << PRIMES.Length) - 1; mask > 0; --mask)
+                {
+                    if ((mask & subset) == subset)
+                    {
+                        f[mask] = (int)((f[mask] + ((long)f[mask ^ subset]) * freq[i]) % MOD);
+                    }
+                }
+            }
+
+            int ans = 0;
+            for (int mask = 1, maskMax = (1 << PRIMES.Length); mask < maskMax; ++mask)
+            {
+                ans = (ans + f[mask]) % MOD;
+            }
+
+            return ans;
+        }
+
+        #endregion
     }
 }
